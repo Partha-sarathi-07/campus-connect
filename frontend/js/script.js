1c9162a0587ecf8ba755e9b3dcc7e5bab98942df
thamil thennarasu <thamilthennarasu@gmail.com>
2025-08-23 01:46:05 +0530

    Integrated sidebar and navbar section

---
+++ frontend/js/script.js
@@ -0,0 +1,17 @@
+const post = document.querySelector(".post-btn");
+const mobile_menu = document.querySelector(".menu-icon");
+const mobile_user_icon = document.querySelector(".user-icon");
+
+post.addEventListener("click",function(){
+  console.log("post");
+})
+
+mobile_menu.addEventListener("click",function(){
+  console.log("menu");
+  
+})
+
+mobile_user_icon.addEventListener("click",function(){
+  console.log("user");
+  
+})
