cb41f3dd36b2569b289c1d4a088a4c1722922a09
thamil thennarasu <thamilthennarasu@gmail.com>
2025-08-24 01:17:11 +0530

    Integrade hero section and sidebar and add post card

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,17 +1,44 @@
+
+const post_container = document.querySelector(".post-container");
+const post_cancel = document.querySelector(".cancel-btn");
+const upload_post = document.querySelector(".post-upload-btn");
 const post = document.querySelector(".post-btn");
-const mobile_menu = document.querySelector(".menu-icon");
 const mobile_user_icon = document.querySelector(".user-icon");
+const search = document.querySelector(".search-events");
 
-post.addEventListener("click",function(){
+// post button
+post.addEventListener("click", function () {
   console.log("post");
+  post_container.style.display = "flex"
 })
 
-mobile_menu.addEventListener("click",function(){
-  console.log("menu");
-  
+// post cancel
+post_cancel.addEventListener("click", function () {
+  console.log("cencel post");
+  post_container.style.display = "none"
+
 })
 
-mobile_user_icon.addEventListener("click",function(){
+//post upload
+upload_post.addEventListener("click", function () {
+  const textvalue = document.querySelector(".text-area").value.trim();
+  console.log(textvalue);
+  if (textvalue === "") {
+    console.log("post Empty ");
+  }else{
+    console.log("post uploated");
+    
+  }
+})
+
+// mobile user icon
+mobile_user_icon.addEventListener("click", function () {
   console.log("user");
-  
+
 })
+
+// search icon
+search.addEventListener("keydown", function (event) {
+  console.log(event.key);
+
+}) 
